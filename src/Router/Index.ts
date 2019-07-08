3270fbbed329303986fe493fc5044023c4fba65a
yejunhong <123241792@qq.com>
2019-07-08 17:48:03 +0800

    .

--- src/Router/Index.ts
+++ src/Router/Index.ts
@@ -1,8 +1,19 @@
+import * as React from 'react';
+import { Router, Switch, Route } from 'react-router';
 import Main from '../Views/Main';
 import Login from '../Views/Login';
 import Test from '../Views/Test';
 import Test1 from '../Views/Test1';
-const router = [
+
+interface RouterParam {
+  path?: string;
+  exact?: boolean;
+  auth?: string;
+  component?: React.ReactNode;
+  children?: RouterParam[];
+}
+
+const routes: RouterParam[] = [
   {
     path: '/login',
     exact: true,
@@ -12,17 +23,17 @@ const router = [
     path: '/test1',
     exact: true,
     component: Main,
-    routes: [
+    children: [
       {
         path: '/test1/admin',
         exact: true,
-        component: Test
+        component: Test,
       }
     ]
   },
   {
     component: Main,
-    routes: [
+    children: [
       {
         path: '/',
         exact: true,
@@ -39,5 +50,27 @@ const router = [
     ]
   }
 ];
-
-export default router;
+/*
+const Router: React.FC = () => {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Main>
+          <Route exact={true} path="/" component={Test} />
+          <Route exact={true} path="/test" component={Test} />
+          <Route exact={true} path="/test1" component={Test1} />
+          <Route path="*" component={Test} />
+        </Main>
+      </Switch>
+    </HashRouter>
+  );
+};*/
+export default React.createElement(Switch, {}, routes.map((route: RouterParam, k: number) => {
+  React.createElement(Route, {
+    render(): React.ReactNode {
+      return route.component;
+    }
+  });
+  return 1;
+}));;
