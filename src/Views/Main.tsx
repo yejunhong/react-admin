33edc0215f337f34e3bb58de06624f01db30b779
yejunhong <123241792@qq.com>
2019-07-09 18:11:37 +0800

    .

--- src/Views/Main.tsx
+++ src/Views/Main.tsx
@@ -14,10 +14,11 @@ const CustomMenu: React.FC = () => {
     <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
       <Menu.Item key="1">
         <Icon type="user" />
-        <span>nav 1</span>
+        <span>{count}</span>
       </Menu.Item>
       <Menu.Item key="2">
         <Icon type="video-camera" />
+        <span>{count}</span>
       </Menu.Item>
       <Menu.Item key="3">
         <Icon type="upload" />
